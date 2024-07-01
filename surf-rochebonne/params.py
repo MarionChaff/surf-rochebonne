0ecf6ad15e2d22e917ba7328a253698aced2ef9b
Marion Chaffaut <marion.chaffaut@gmail.com>
2024-07-01 13:19:53 +0200

    01/07/24

---
+++ surf-rochebonne/params.py
@@ -0,0 +1,12 @@
+#import os
+#import numpy as np
+
+##################  VARIABLES  ##################
+
+WG_URL = 'https://www.windguru.cz/112'
+NUM_PREV = 100
+
+##################  CONSTANTS  ##################
+
+LOCAL_DATA_PATH = ''
+EXEC_PATH = '/Users/marionchaffaut/chromedriver/chromedriver-mac-x64/chromedriver'
--- surf-rochebonne/scrape/scraper.py
+++ surf-rochebonne/scrape/scraper.py
@@ -2,6 +2,9 @@
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import TimeoutException
 
 # params
 from ..params import *
@@ -12,7 +15,8 @@ class Scraper:
 
     def __init__(self, url):
         self.url = url
-        self.driver = webdriver.Chrome(executable_path=EXEC_PATH)
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.url)
 
     def scrape(self, num_prev):
 
@@ -27,33 +31,58 @@ class Scraper:
             - Swell height, period and direction
         """
 
-        self.driver.get(self.url)
+        # Wait for the browsed page before scraping
+        try:
+            myElem = WebDriverWait(self.driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, '//*[@id="tabid_0_0_dates"]/td[1]')))
+        except TimeoutException:
+            None
+
         forecast = {}
 
-        names_fig = ['tabid_0_0_dates','tabid_0_0_WINDSPD','tabid_0_0_GUST','tabid_0_0_HTSGW', 'tabid_0_0_PERPW']
-        names_dir = ['tabid_0_0_SMER','tabid_0_0_DIRPW']
+        parse_number = lambda x: int(''.join([l for l in str(x) if l.isdigit()]))
 
-        for name in names_fig:
+        # Extract datetime
+        temp_list = []
+        for i in range(1, num_prev + 1):
+            try:
+                value = self.driver.find_element(By.XPATH, f'//*[@id="tabid_0_0_dates"]/td[{i}]')
+                temp_list.append(value.text)
+            except Exception as e:
+                temp_list.append(pd.NA)
+        forecast['date'] = temp_list
+
+        # Extract numeric figures
+        for name in ['tabid_0_0_WINDSPD','tabid_0_0_GUST','tabid_0_0_HTSGW', 'tabid_0_0_PERPW'] :
             temp_list = []
             for i in range(1, num_prev + 1):
                 try:
                     value = self.driver.find_element(By.XPATH, f'//*[@id="{name}"]/td[{i}]')
-                    temp_list.append(value.text)
+                    numeric_value = float(value.text.strip())  # Convert text to float
+                    temp_list.append(numeric_value)
                 except Exception as e:
-                    temp_list.append('na')
+                    temp_list.append(pd.NA)
             forecast[name] = temp_list
 
-        for name in names_dir:
+        # Extract angles
+        for name in ['tabid_0_0_SMER','tabid_0_0_DIRPW']:
             temp_list = []
             for i in range(1, num_prev + 1):
                 try:
                     value = self.driver.find_element(By.XPATH, f'//*[@id="{name}"]/td[{i}]/span')
-                    temp_list.append(value.get_attribute('title'))
+                    numeric_value = parse_number(value.get_attribute('title'))
+                    temp_list.append(numeric_value)
                 except Exception as e:
-                    temp_list.append('na')
+                    temp_list.append(pd.NA)
             forecast[name] = temp_list
 
         forecast_df=pd.DataFrame(forecast)
+
+        # Formatting
+        forecast_df.dropna(inplace=True)
+        forecast_df.columns = ['date','wind_speed','gust_speed','swell_height','swell_period','wind_dir','swell_dir']
+        forecast_df['wind_speed'] = forecast_df[['wind_speed', 'gust_speed']].mean(axis=1)
+        forecast_df = forecast_df.drop(columns=['gust_speed'])
+        forecast_df[['wind_speed','swell_period']] = forecast_df[['wind_speed','swell_period']].astype(int)
 
         return forecast_df
 
